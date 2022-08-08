35d730fe723a1e93976b070ac28f9d6ffc96ab6a
Daniel Haarhoff <daniel@rknt.de>
2022-08-08 10:20:43 +0100

    avoid non-nord bright colours on bold

--- config.h
+++ config.h
@@ -106,14 +106,14 @@ static const char *colorname[] = {
 	"#e5e9f0", /* white   */
 
 	/* 8 bright colors */
-	"#4c566a", /* black   */
+	"#3b4252", /* black   */
 	"#bf616a", /* red     */
 	"#a3be8c", /* green   */
 	"#ebcb8b", /* yellow  */
 	"#81a1c1", /* blue    */
 	"#b48ead", /* magenta */
-	"#8fbcbb", /* cyan    */
-	"#eceff4", /* white   */
+	"#88c0d0", /* cyan    */
+	"#e5e9f0", /* white   */
 
 	[255] = 0,
 
